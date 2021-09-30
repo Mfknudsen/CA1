6dfe37a4f2fd753bee8bde6d097d69e50a297788
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-09-30 15:45:14 +0200

    Updated relevant variables + temporary default NamedQueries

--- src/main/java/entities/Person.java
+++ src/main/java/entities/Person.java
@@ -1,13 +1,89 @@
 package entities;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import java.io.Serializable;
+import java.util.List;
 
 @Entity
+@Table(name = "Person")
+@NamedQueries({
+        @NamedQuery(name = "Person.deleteAllRows", query = "DELETE FROM Person"),
+        @NamedQuery(name = "Person.getAll", query = "SELECT p FROM Person p")
+})
 public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
     @Id
-    @Column(name = "id", nullable = false)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="firstName")
+    private String firstName;
+
+    @Column(name="lastName")
+    private String lastName;
+
+    @OneToMany(mappedBy = "person", cascade = CascadeType.PERSIST)
+    private List<Phone> phones;
+
+    @ManyToMany(mappedBy = "persons", cascade = CascadeType.PERSIST)
+    private List<Hobby> hobbies;
+
+    @ManyToOne
+    @JoinColumn(name = "hobby_id")
+    private Hobby hobby;
+
+    public Person() {}
+
+    public Person(String email, String firstName, String lastName) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    
+
+    public long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
