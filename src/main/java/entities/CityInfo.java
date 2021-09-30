d5f240ad3c84566f1dee377db1de8d325cbcfd17
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-09-30 15:46:31 +0200

    Added CityInfo.java

---
+++ src/main/java/entities/CityInfo.java
@@ -0,0 +1,69 @@
+package entities;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "CityInfo")
+@NamedQueries({
+        @NamedQuery(name = "CityInfo.deleteAllRows", query = "DELETE FROM CityInfo"),
+        @NamedQuery(name = "CityInfo.getAll", query = "SELECT c FROM CityInfo c")
+})
+public class CityInfo implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private long id;
+
+    @Column(name="zipCode")
+    private String zipCode;
+
+    @Column(name="city")
+    private String city;
+
+    @OneToMany(mappedBy = "cityInfo", cascade = CascadeType.PERSIST)
+    private List<Address> addresses;
+
+    public CityInfo() {}
+
+    public CityInfo(String zipCode, String city) {
+        this.zipCode = zipCode;
+        this.city = city;
+    }
+    
+    public long getId() {
+        return id;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+}
+
+
+
