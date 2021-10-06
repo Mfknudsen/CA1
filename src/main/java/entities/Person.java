96cd40a82f8396227c44bbacbde2cfea728b3b1d
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-10-06 16:26:17 +0200

    Fixed typo in addHobby()

--- src/main/java/entities/Person.java
+++ src/main/java/entities/Person.java
@@ -68,7 +68,7 @@ public class Person implements Serializable {
     }
 
     public void addHobby(Hobby hobby) {
-        if (hobbies != null) {
+        if (hobby != null) {
             this.hobbies.add(hobby);
             hobby.addPerson(this);
         }
